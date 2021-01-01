65224ef5d678e9330f40cf8fd5deea718b3ce62b
Patrick Creutzburg <itchimonji@Yuna-MB-Pro.fritz.box>
2021-01-02 00:30:56 +0100

    add test for abstract factory

---
+++ src/app/01. Abstract Factory/abstract-factory/abstract-factory.component.spec.ts
@@ -0,0 +1,50 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularMaterialModule } from '../../modules/angular-material.module';
+import { AbstractFactoryComponent } from './abstract-factory.component';
+import { AbstractTheme } from '../models/theme.model';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('CommandComponent', () => {
+  let component: AbstractFactoryComponent;
+  let fixture: ComponentFixture<AbstractFactoryComponent>;
+
+  let lightTheme: AbstractTheme;
+  let darkTheme: AbstractTheme;
+  let personalTheme: AbstractTheme;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AbstractFactoryComponent],
+      imports: [BrowserAnimationsModule, FormsModule, AngularMaterialModule]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AbstractFactoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    const themes = component.themes;
+    lightTheme = Object.assign(Object.create(themes[0]), themes[0]);
+    darkTheme = Object.assign(Object.create(themes[1]), themes[1]);
+    personalTheme = Object.assign(Object.create(themes[2]), themes[2]);
+  });
+
+  it('should have a light theme', () => {
+    expect(lightTheme.name).toEqual('Light theme');
+    expect(lightTheme.getStyleName()).toEqual('light');
+    expect(lightTheme.getFontName()).toEqual('Comic Sans MS');
+  });
+
+  it('should have a dark theme', () => {
+    expect(darkTheme.name).toEqual('Dark theme');
+    expect(darkTheme.getStyleName()).toEqual('dark');
+    expect(darkTheme.getFontName()).toEqual('Arial Black');
+  });
+
+  it('should have a personal theme', () => {
+    expect(personalTheme.name).toEqual('Personal theme');
+    expect(personalTheme.getStyleName()).toEqual('personal');
+    expect(personalTheme.getFontName()).toEqual('Times New Roman');
+  });
+});
--- src/app/01. Abstract Factory/abstract-factory/abstract-factory.component.ts
+++ src/app/01. Abstract Factory/abstract-factory/abstract-factory.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Theme } from '../models/theme.model';
+import { AbstractTheme, Theme } from '../models/theme.model';
 import { DarkThemeFactory, LightThemeFactory, PersonalThemeFactory } from '../models/factory.model';
 
 @Component({
@@ -8,22 +8,25 @@ import { DarkThemeFactory, LightThemeFactory, PersonalThemeFactory } from '../mo
   styleUrls: ['./abstract-factory.component.scss']
 })
 export class AbstractFactoryComponent implements OnInit {
-  public themes: Theme[];
-  public selectedTheme: Theme;
+  private readonly _themes: AbstractTheme[];
+  public get themes(): AbstractTheme[] {
+    return this._themes;
+  }
+  public selectedTheme: AbstractTheme;
 
   constructor() {
-    this.themes = new Array<Theme>();
+    this._themes = new Array<AbstractTheme>();
   }
 
   ngOnInit(): void {
     // I split this up for better readability
-    const lightTheme: Theme = new Theme(new LightThemeFactory(), 'Light theme');
-    this.themes.push(lightTheme);
-    const darkTheme: Theme = new Theme(new DarkThemeFactory(), 'Dark theme');
-    this.themes.push(darkTheme);
-    const personalTheme: Theme = new Theme(new PersonalThemeFactory(), 'Personal theme');
-    this.themes.push(personalTheme);
+    const lightTheme: AbstractTheme = new Theme(new LightThemeFactory(), 'Light theme');
+    this._themes.push(lightTheme);
+    const darkTheme: AbstractTheme = new Theme(new DarkThemeFactory(), 'Dark theme');
+    this._themes.push(darkTheme);
+    const personalTheme: AbstractTheme = new Theme(new PersonalThemeFactory(), 'Personal theme');
+    this._themes.push(personalTheme);
 
-    this.selectedTheme = this.themes[0];
+    this.selectedTheme = this._themes[0];
   }
 }
